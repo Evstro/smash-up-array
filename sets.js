d8289c848fbd9d447e0bfb974bef57e8e9f1ceee
Evstro <Evstro.code@gmail.com>
2022-06-10 14:27:51 -0500

    Smash Up Array
    Goal is to create an array of factions based on sets owned
    
    [Ticket: X]

---
+++ sets.js
@@ -0,0 +1,20 @@
+let base = [
+    'Aliens',
+    'Dinosaurs',
+    'Ninjas',
+    'Pirates',
+    'Robots',
+    'Tricksters',
+    'Wizards',
+    'Zombies'
+];
+
+let al9000 = [
+    'Bear Cavalry',
+    'Ghosts',
+    'Killer Plants',
+    'Steampunks'
+]
+
+let sets = [base + al9000];
+
