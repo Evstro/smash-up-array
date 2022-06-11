0c0a342db0ffd7aec6b13c11e84e1aab32dbcb94
Evstro <Evstro.code@gmail.com>
2022-06-10 21:58:39 -0500

    Faction List Creator and Random Faction Generator
    Created functions to let the user choose
    sets to generate a list of factions and
    pick two random factions
    
    [Ticket: X]

--- sets.js
+++ sets.js
@@ -1,4 +1,6 @@
-let base = [
+const setFactions = new Map();
+
+setFactions.set('base', [
     'Aliens',
     'Dinosaurs',
     'Ninjas',
@@ -7,14 +9,177 @@ let base = [
     'Tricksters',
     'Wizards',
     'Zombies'
-];
+]);
 
-let al9000 = [
+setFactions.set('al9000', [
     'Bear Cavalry',
     'Ghosts',
     'Killer Plants',
     'Steampunks'
-]
+]);
 
-let sets = [base + al9000];
+setFactions.set('cthulhu', [
+    'Elder Things',
+    'Innsmouth',
+    'Minions of Cthulhu',
+    'Miskatonic University'
+]);
 
+setFactions.set('scifidbl', [
+    'Cyborg Apes',
+    'Shapeshifters',
+    'Super Spies',
+    'Time Travelers'
+]);
+
+setFactions.set('monster', [
+    'Giant Ants',
+    'Mad Scientists',
+    'Vampires',
+    'Werewolves'
+]);
+
+setFactions.set('pretty', [
+    'Fairies',
+    'Kitty Cats',
+    'Mythic Horses',
+    'Princesses'
+]);
+
+setFactions.set('urfault', [
+    'Dragons',
+    'Mythic Greeks',
+    'Sharks',
+    'Superheroes',
+    'Tornadoes'
+]);
+
+setFactions.set('cease', [
+    'Astroknights',
+    'Changerbots',
+    'Ignobles',
+    'Star Roamers'
+]);
+
+setFactions.set('thinking', [
+    'Explorers',
+    'Grannies',
+    'Rock Stars',
+    'Teddy Bears'
+]);
+
+setFactions.set('japan', [
+    'Itty Critters',
+    'Kaiju',
+    'Magical Girls',
+    'Mega Troopers'
+ ]);
+
+setFactions.set('seventies', [
+    'Disco Dancers',
+    'Kung Fu Fighters',
+    'Truckers',
+    'Vigilantes'
+ ]);
+
+setFactions.set('oops', [
+    'Ancient Egyptians',
+    'Cowboys',
+    'Samurai',
+    'Vikings'
+ ]);
+
+setFactions.set('wtint', [
+    'Luchadores',
+    'Mounties',
+    'Musketeers',
+    'Sumo Wrestlers'
+ ]);
+
+setFactions.set('wtshock', [
+    'Anansi Tales',
+    'Ancient Incas',
+    'Grimms Fairy Tales',
+    'Polynesian Voyagers',
+    'Russian Fairy Tales'
+ ]);
+
+setFactions.set('sheep', [
+    'Sheep'
+ ]);
+
+setFactions.set('penguins', [
+    'Penguins'
+ ]);
+
+setFactions.set('goblins', [
+    'Goblins'
+ ]);
+
+setFactions.set('knights', [
+    'Knights of the Round Table'
+ ]);
+
+setFactions.set('geeks', [
+    'Geeks'
+ ]);
+
+setFactions.set('allstars', [
+    'All Stars'
+ ]);
+
+setFactions.set('munch', [
+    'Clerics',
+    'Dwarves',
+    'Elves',
+    'Halflings',
+    'Mages',
+    'Orcs',
+    'Thieves',
+    'Warriors'
+ ]);
+
+setFactions.set('marvel', [
+    'Avengers',
+    'Hydra',
+    'Kree',
+    'Masters of Evil',
+    'S.H.I.E.L.D.',
+    'Sinister Six',
+    'Spider-Verse',
+    'Ultimates'
+ ]);
+
+setFactions.set('dis', [
+    'Aladdin',
+    'Beauty and the Beast',
+    'Big Hero 6',
+    'Frozen',
+    'Mulan',
+    'The Lion King',
+    'The Nightmare Before Christmas',
+    'Wreck-It Ralph'
+ ]);
+
+//  let sets = base + 
+//  al9000 + 
+//  cthulhu + 
+//  scifidbl + 
+//  monster + 
+//  pretty + 
+//  urfault + 
+//  cease + 
+//  thinking + 
+//  japan + 
+//  seventies + 
+//  oops + 
+//  wtint + 
+//  wtshock + 
+//  sheep + 
+//  goblins + 
+//  knights + 
+//  geeks + 
+//  allstars + 
+//  munch + 
+//  marvel + 
+//  dis;
